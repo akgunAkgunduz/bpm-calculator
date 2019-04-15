5d6a7c6468d5bd8e2066d0136b3a0afe667cb675
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-04-15 11:12:37 +0300

    Add defaultSettings, initApp and localStorage

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ const bpm = document.querySelector('#bpm')
 const speed = document.querySelector('#speed')
 const table = document.querySelector('#bpm-table')
 
+const defaultSettings = {
+  bpm: 180,
+  groups: 4
+}
+
 function calculate(groupValue, bpmValue) {
   return bpmValue * groupValue / 60
 }
@@ -34,16 +39,33 @@ function drawTable() {
   }
 }
 
+function initApp() {
+  if (localStorage.bpmCalculatorBpm) {
+    bpm.value = parseInt(localStorage.bpmCalculatorBpm)
+  } else {
+    bpm.value = defaultSettings.bpm
+  }
+
+  if (localStorage.bpmCalculatorGroups) {
+    groups.value = parseInt(localStorage.bpmCalculatorGroups)
+  } else {
+    groups.value = defaultSettings.groups
+  }
+}
+
 bpm.addEventListener('input', (e) => {
+  localStorage.setItem('bpmCalculatorBpm', e.target.value)
   speed.innerHTML = calculate(groups.options[groups.selectedIndex].value, e.target.value).toFixed(3)
   drawTable()
 })
 
 groups.addEventListener('input', (e) => {
+  localStorage.setItem('bpmCalculatorGroups', e.target.value)
   speed.innerHTML = calculate(groups.options[groups.selectedIndex].value, bpm.value).toFixed(3)
   drawTable()
 })
 
 document.addEventListener('DOMContentLoaded', () => {
+  initApp()
   drawTable()
 })
